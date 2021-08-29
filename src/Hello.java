64819f1bb285dad84842c17842bda596bc8b698d
cnichols <cnichols@rcentral.org>
2021-08-29 13:18:02 -0500

    Add CN lines

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,9 @@ public class Hello {
 	public static void main(String args[]) {
 		System.out.println("Elijah Schoneweis");
 		System.out.println("Computer Science");
+
+		System.out.println("Connor Nichols!");
+		System.out.println("Computer Science!");
 	}
 	
 }
